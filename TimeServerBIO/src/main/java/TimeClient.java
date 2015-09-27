fa4ccbb79ec6cf02717bb31b7a22775d32ee6ddd
XIN <xsuhust@gmail.com>
2015-09-27 15:08:38 -0700

    ADD time client

--- TimeServerBIO/src/main/java/TimeClient.java
+++ TimeServerBIO/src/main/java/TimeClient.java
@@ -20,22 +20,40 @@ public class TimeClient {
         }
 
 
-        Socket socket;
-        BufferedReader in;
-        PrintWriter out;
+        Socket socket =null;
+        BufferedReader in = null;
+        PrintWriter out = null;
 
         try {
             socket = new Socket("localhost", port);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
             out.println("QUERY TIME ORDER");
-
-
-
-            
+            System.out.println("Send order to server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now is : " + resp);
          } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if(out != null) {
+                out.close();
+                out = null;
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                socket = null;
+            }
         }
-
     }
 }
