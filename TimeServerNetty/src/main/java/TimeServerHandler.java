06d62b7e06bb8090a423c2c4382ac18f0f1ec997
XIN <xsuhust@gmail.com>
2015-10-07 20:52:23 -0700

    create a case of pressure test. In this case, it will suffer from TCP adhering issue. Update

--- TimeServerNetty/src/main/java/TimeServerHandler.java
+++ TimeServerNetty/src/main/java/TimeServerHandler.java
@@ -20,18 +20,21 @@ import java.util.Date;
  */
 public class TimeServerHandler extends ChannelHandlerAdapter {
 
+    private int counter;
+
     @Override
     public void channelRead (ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-        String body = new String(req, "UTF-8");
+        String body = new String(req, "UTF-8").substring(0,req.length - System.getProperty("line.separator").length());
 
-        System.out.println("The Time Server receive order : " + body);
+        System.out.println("The Time Server receive order : " + body + "the counter is : " + ++counter);
 
         String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(
                 System.currentTimeMillis()
         ).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
         ctx.write(resp);
     }
