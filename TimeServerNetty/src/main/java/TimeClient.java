eda14175e3e73ead92eae8c87815928e80810262
XIN <xsuhust@gmail.com>
2015-10-07 21:12:38 -0700

    加入LineBasedFrameDecoder 和 StringDecoder, 解决了TCP 粘包的问题.读取InputStream的时候也不需要编解码 直接读取msg就可以了

--- TimeServerNetty/src/main/java/TimeClient.java
+++ TimeServerNetty/src/main/java/TimeClient.java
@@ -6,7 +6,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 
 /**
@@ -24,6 +25,8 @@ public class TimeClient {
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            ch.pipeline().addLast(new StringDecoder());
                             ch.pipeline().addLast(new TimeClientHandler());
                         }
                     });
